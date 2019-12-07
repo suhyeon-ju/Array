07326f1c9082bc38532e0ab52ca8b887c7aedc8c
suhyeon-ju <57384796+suhyeon-ju@users.noreply.github.com>
2019-12-07 22:57:22 +0900

    Array exercise

---
+++ Array/Array.js
@@ -0,0 +1,18 @@
+const juInfo ={
+  name : "su",
+  age : 33,
+  gender : "Male",
+  isHandsome: true
+  favMovie : ["NottingHill", "Jumper", "AboutTime"],
+  favFood: [
+    {
+      name: "Cheese burger",
+      fatty: true
+    },
+    {
+      name: "Rice",
+      fatty: true
+    }
+  ]
+}
+console.log(juInfo)
